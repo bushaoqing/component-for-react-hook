bdedf4c51485a5fb718c69886cd622ae3552fdac
jasonbu <jasonbu@tencent.com>
2021-07-17 16:15:04 +0800

    add copyText comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Buttom from './components/Button'
+import CopyText from './components/CopyText'
 
 // 测试容器
 function App() {
   return (
     <div className="App">
       <Buttom text="aaa" type="submit" />
+
+      <hr />
+
+      我就是你要得复制内容<CopyText copyText="我就是你要得复制内容" />
     </div>
   );
 }
---
+++ src/components/CopyText/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import USERIMG from './img/icon_copy@2x.png'
+import './index.css'
+
+function CopyText(props) {
+
+  let [isCopy, setIsCopy] = useState(false)
+  let [isShowTip, setIsShowTip] = useState(false)
+
+  function handleCopy () {
+
+    !!props.copyText && copyToClipboard(props.copyText)
+    setIsCopy(true)
+  }
+
+  return (
+    <div style={{ position: 'absolute', display: 'inline-block' }}>
+      <img 
+        className="comp-copy-text__user-img" 
+        src={USERIMG}
+        onClick={handleCopy}
+        onMouseLeave={() => {
+          setIsCopy(false)
+          setIsShowTip(false)
+        }}
+        onMouseEnter={() => setIsShowTip(true)}
+      ></img>
+      {
+        isShowTip && <div className="suspend">
+          <span>{isCopy ? '复制成功' : !!props.copyTip ? props.copyTip : '点击复制'}</span>
+        </div>
+      }
+    </div>
+  )
+}
+
+function copyToClipboard(value) {
+  const input = document.createElement('input')
+  input.style.display= 'none'
+  input.value = value
+  document.body.append(input)
+  input.select()
+  document.execCommand('copy')
+  input.parentNode.removeChild(input)
+}
+
+CopyText.propTypes = {
+  copyTip: PropTypes.string,
+  isShowTip: PropTypes.bool
+}
+
+export default CopyText
