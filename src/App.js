fd43ff39968a05689a3c6315826e6f733f24476f
jasonbu <jasonbu@tencent.com>
2021-07-19 11:14:23 +0800

    update react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link, BrowserRouter, Redirect } from 'react-router-dom'
 import { RouteMap } from './router'
 
 // 测试容器
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <ol className='side_menu_wrap'>
         {
           RouteMap.map(item => (
@@ -21,11 +21,12 @@ function App() {
           {
             RouteMap.map(item => (
               <Route key={item.path} path={item.path} component={item.component}></Route>
-            ))
-          }
+              ))
+            }
+          <Redirect to={RouteMap[0].path} from='*' />
         </Switch>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
